9173c707b8f8076774a928a5780a805966ecc5f4
cthom3 <129532871+cthom3@users.noreply.github.com>
2025-02-06 23:15:23 -0700

    Working on validMoves function which currently passes two test in ValidMoves test set. Fixed the error of returning null

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -56,13 +56,13 @@ public class ChessGame {
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         ChessPiece piece=currentBoard.getPiece(startPosition);
+        Collection<ChessMove> acceptableMoves = new ArrayList<>();
         if (piece==null){
-            return null;
+            return acceptableMoves;
         } else {
             if (piece.getTeamColor()==getTeamTurn()) {
                 ChessBoard copyBoard=currentBoard.DeepCopy();
                 Collection<ChessMove> potentialMoves = piece.pieceMoves(currentBoard, startPosition);
-                Collection<ChessMove> acceptableMoves = new ArrayList<>();
                 for (ChessMove move : potentialMoves) {
                     if (isInCheck(piece.getTeamColor()) == false) {
                         ChessPosition newPosition=move.getEndPosition();
@@ -76,7 +76,7 @@ public class ChessGame {
                 }
                 return acceptableMoves;
             } else {
-                return null;
+                return acceptableMoves;
             }
         }
     }
