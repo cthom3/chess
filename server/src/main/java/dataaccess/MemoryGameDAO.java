c7e0d0a94dd41f782bac6b86f110f48105002db2
cthom3 <129532871+cthom3@users.noreply.github.com>
2025-02-28 19:01:38 -0700

    Added necessary functions for memory game dataAccess

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,12 +1,37 @@
 package dataaccess;
 import model.GameData;
 import chess.ChessGame;
+import model.UserData;
+
+import java.util.Collection;
+import java.util.HashMap;
 
 public class MemoryGameDAO implements GameDAO {
+    final private HashMap<Integer, GameData> games = new HashMap<>();
     private int nextId = 1;
     public void createGame(String gameName){
         ChessGame newGame= new ChessGame();
         GameData game=new GameData(nextId, null,null,gameName, newGame);
+        games.put(game.gameID(),game);
         nextId+=1;
     }
+
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    public Collection<GameData> listGames(){
+        return games.values();
+    }
+
+    public GameData updateGame (int gameID){
+        GameData originalGame=games.get(gameID);
+        GameData updatedGame=new GameData(
+                originalGame.gameID(),originalGame.whiteUsername(),
+                originalGame.blackUsername(), originalGame.gameName(),
+                originalGame.game());
+        games.put(gameID,updatedGame);
+        return updatedGame;
+    }
+
 }
