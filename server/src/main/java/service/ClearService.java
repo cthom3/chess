8b5afaf0defd9e9f0d8003be80aecd21dc6d8264
cthom3 <129532871+cthom3@users.noreply.github.com>
2025-03-01 18:34:21 -0700

    Created ClearService and ClearRequest and ClearResult records in order to delete everything

---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,27 @@
+package service;
+import dataaccess.UserDAO;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.DataAccessException;
+
+public class ClearService {
+    private final UserDAO userAccess;
+    private final AuthDAO authAccess;
+    private final GameDAO gameAccess;
+    public ClearService(UserDAO userAccess, AuthDAO authAccess, GameDAO gameAccess){
+        this.userAccess=userAccess;
+        this.authAccess=authAccess;
+        this.gameAccess=gameAccess;
+    }
+    public ClearResult clear(ClearRequest clearrequest){
+        try {
+            gameAccess.clear();
+            authAccess.clear();
+            userAccess.clear();
+            return new ClearResult (null);
+        } catch (DataAccessException ex){
+            return new ClearResult (ex.getMessage());
+        }
+
+    }
+}
