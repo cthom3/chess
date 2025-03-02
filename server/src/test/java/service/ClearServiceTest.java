c5f6b38bf47e063f1e903ba6e7e3cc30545d72ee
cthom3 <129532871+cthom3@users.noreply.github.com>
2025-03-01 20:59:11 -0700

    Completed RegisterPositiveTest

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -2,6 +2,7 @@ package service;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import dataaccess.DataAccessException;
+import service.ClearService;
 
 public class ClearServiceTest {
     @Test
--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,25 +1,42 @@
 package service;
+import dataaccess.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import dataaccess.DataAccessException;
+
+import service.UserService;
+import model.UserData;
+import model.AuthData;
+import dataaccess.UserDAO;
+import dataaccess.AuthDAO;
 
 public class UserServiceTest {
+    private final UserDAO userAccess=new MemoryUserDAO();
+    private final AuthDAO authAccess=new MemoryAuthDAO();
+    final UserService service = new UserService(userAccess, authAccess);
     @Test
     public void registerPositiveTest() throws DataAccessException{
+        UserData expectedUser=new UserData("username", "password", "username@email.com");
+        service.register(new RegisterRequest("username", "password", "username@email.com"));
+        UserData actualUser=userAccess.getUser("username");
+        assertEquals(expectedUser,actualUser);
+    }
+    @Test
+    public void registerNegativeTest() throws DataAccessException{
 
     }
-    public void registerNegativeTest()throws DataAccessException{
-
-    }
+    @Test
     public void loginPositiveTest() throws DataAccessException{
 
     }
+    @Test
     public void loginNegativeTest() throws DataAccessException{
 
     }
+    @Test
     public void logoutPositiveTest() throws DataAccessException{
 
     }
+    @Test
     public void logoutNegativeTest() throws DataAccessException{
 
     }
