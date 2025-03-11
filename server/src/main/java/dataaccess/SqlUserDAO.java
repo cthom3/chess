565430962ab410e041016cb91ab649da84065c50
cthom3 <129532871+cthom3@users.noreply.github.com>
2025-03-10 22:45:38 -0600

    Started writing functions to SQL DAO files, initialization functions

--- server/src/main/java/dataaccess/SqlUserDAO.java
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -8,8 +8,8 @@ import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static java.sql.Types.NULL;
 
 public class SqlUserDAO implements UserDAO {
-    public SqlUserDAO() {
-
+    public SqlUserDAO() throws DataAccessException,SQLException {
+        configureDatabase();
     }
     public void createUser(String username, String password, String email) throws DataAccessException,SQLException {
         try (var conn=DatabaseManager.getConnection()){
@@ -71,6 +71,29 @@ public class SqlUserDAO implements UserDAO {
                 return 0;
             }catch (SQLException e) {
                 throw new DataAccessException(e.getMessage());
+            }
+        } catch (DataAccessException e){
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private final String[] createStatements={
+            """
+            CREATE TABLE IF NOT EXISTS user(
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL
+            )
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException,SQLException {
+        DatabaseManager.createDatabase();
+        try (var conn=DatabaseManager.getConnection()){
+            for (var statement:createStatements){
+                try (var preparedStatement= conn.prepareStatement(statement)){
+                    preparedStatement.executeUpdate();
+                }
             }
         } catch (DataAccessException e){
             throw new DataAccessException(e.getMessage());
