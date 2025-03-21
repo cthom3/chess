c3628abe3ba1b4b576bfc0bc3a5e32f78f3f8fd7
cthom3 <129532871+cthom3@users.noreply.github.com>
2025-03-20 19:07:41 -0600

    Finished ServerFacade Functions

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,6 +1,7 @@
 package ui;
 
 
+import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import service.userrecords.*;
 import service.gamerecords.*;
@@ -20,33 +21,33 @@ public class ServerFacade {
         return this.makeRequest("POST", path, request, RegisterResult.class);
     }
 
-    public LoginResult login(LoginRequest request){
+    public LoginResult login(LoginRequest request) throws DataAccessException {
         var path="/session";
         return this.makeRequest("POST", path, request, LoginResult.class);
 
     }
 
-    public LogoutResult logout(LogoutRequest request){
+    public LogoutResult logout(LogoutRequest request) throws DataAccessException {
         var path="/session";
         return this.makeRequest("DELETE", path, request, LogoutResult.class);
     }
 
-    public CreateGameResult createGame (CreateGameRequest request){
+    public CreateGameResult createGame (CreateGameRequest request) throws DataAccessException {
         var path="/game";
         return this.makeRequest("POST", path, request, CreateGameResult.class);
     }
 
-    public JoinGameResult joinGame (JoinGameRequest request){
+    public JoinGameResult joinGame (JoinGameRequest request) throws DataAccessException {
         var path="/game";
         return this.makeRequest("PUT", path, request, JoinGameResult.class);
     }
 
-    public ListGamesResult listGames (ListGamesRequest request){
+    public ListGamesResult listGames (ListGamesRequest request) throws DataAccessException {
         var path="/game";
         return this.makeRequest("GET", path, request, ListGamesResult.class);
     }
 
-    public ClearResult clear (ClearRequest request){
+    public ClearResult clear (ClearRequest request) throws DataAccessException {
         var path="/db";
         return this.makeRequest("DELETE", path, request, ClearResult.class);
     }
@@ -55,9 +56,37 @@ public class ServerFacade {
         try {
             URL url=(new URI(serverUrl+path)).toURL();
             HttpURLConnection http=(HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+            writeRequestBody(request,http);
+            http.connect();
+            return readRequestBody(http,responseClass);
         } catch (Exception e){
            throw new DataAccessException(e.getMessage());
         }
     }
+    private static void writeRequestBody(Object request, HttpURLConnection http) throws IOException {
+        if (request !=null){
+            http.addRequestProperty("Content-Type", "application/json");
+            String requestData=new Gson().toJson(request);
+            try (OutputStream requestBody=http.getOutputStream()){
+                requestBody.write(requestData.getBytes());
+            }
+        }
+    }
+
+    private static <T> T readRequestBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0){
+            try (InputStream responseBody=http.getInputStream()){
+                InputStreamReader reader = new InputStreamReader(responseBody);
+                if (responseClass != null){
+                    response= new Gson().fromJson(reader,responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
 
 }
