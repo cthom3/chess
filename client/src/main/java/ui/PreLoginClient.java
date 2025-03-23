906c5a6f08d0d4ed6375d4f9b09ab3c8edf6b01f
cthom3 <129532871+cthom3@users.noreply.github.com>
2025-03-22 22:44:36 -0600

    Wrote Main, REPL, and PreloginClient functions. Tested functionality

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -1,5 +1,67 @@
 package ui;
+import dataaccess.DataAccessException;
+import service.userrecords.LoginRequest;
+import service.userrecords.LoginResult;
+import service.userrecords.RegisterRequest;
+import service.userrecords.RegisterResult;
+
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class PreLoginClient {
+    private final ServerFacade server;
+    private final String serverUrl;
 
+    public PreLoginClient(String serverUrl){
+        server=new ServerFacade (serverUrl);
+        this.serverUrl=serverUrl;
+    }
+
+    public String eval (String input){
+        try{
+            var tokens=input.toLowerCase().split(" ");
+            var command=(tokens.length>0) ? tokens[0]: "help";
+            var params=Arrays.copyOfRange(tokens,1,tokens.length);
+            return switch (command){
+                case "register" -> register(params);
+                case "login" -> login(params);
+                default -> help();
+            };
+        } catch (Exception ex){
+            return ex.getMessage();
+        }
+    }
+
+    public String register(String... params) throws DataAccessException {
+        if (params.length >= 3) {
+            var username=params[0];
+            var password=params[1];
+            var email= params[2];
+            RegisterRequest request=new RegisterRequest(username, password, email);
+            RegisterResult result=server.register(request);
+            return ("Registration Successful");
+        }
+        return String.format("Error in Registration. Make sure you follow requested format");
+    }
+
+    public String login(String... params) throws DataAccessException{
+
+        if (params.length>=2){
+            var username=params[0];
+            var password=params[1];
+            LoginRequest request=new LoginRequest(username,password);
+            LoginResult result= server.login(request);
+            return String.format("Welcome, %s!", request.username());
+        }
+        return ("Not a registered user");
+    }
+
+    public String help(){
+        return """
+                - register <username password email>
+                - login <username password>
+                - quit
+                - help
+                """;
+    }
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,35 @@
 package ui;
+import java.util.Scanner;
 
 public class Repl {
+    private final PreLoginClient loginClient;
+//    private final PostLoginClient loggedinClient;
+
+    public Repl(String serverUrl){
+        loginClient=new PreLoginClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("Welcome to Chess. Options:");
+        System.out.println(loginClient.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result="";
+        while (!result.equals("quit")){
+            printPrompt();
+            String line=scanner.nextLine();
+            try {
+                result=loginClient.eval(line);
+                System.out.print(result);
+            } catch (Throwable e){
+                System.out.print(e.toString());
+            }
+        }
+        System.out.println();
+
+    }
+
+    private void printPrompt(){
+        System.out.print("\n" + ">>>");
+    }
 }
