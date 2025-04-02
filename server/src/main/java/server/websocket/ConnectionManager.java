e530267607244dedfebc412434660d326cada804
cthom3 <129532871+cthom3@users.noreply.github.com>
2025-04-02 10:39:12 -0600

    added the ability to manage connections and only give messages to users who need them through broadcasting

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -2,13 +2,34 @@ package server.websocket;
 
 import org.eclipse.jetty.websocket.api.Session;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
 
 public class ConnectionManager {
     public final ConcurrentHashMap<String,Connection> connections= new ConcurrentHashMap<>();
 
-    public void add(String visitorName, Session session){
-        var connection = new Connection(visitorName,session);
-        connections.put(visitorName,connection);
+    public void add(String currentUser, Session session){
+        var connection = new Connection(currentUser,session);
+        connections.put(currentUser,connection);
+    }
+
+    public void remove(String currentUser){
+        connections.remove(currentUser);
+    }
+
+    public void broadcast(String excludeUser, Notification notification){
+        ArrayList<Connection> removeList=new ArrayList<Connection>();
+        for (var connection: connections.values()){
+            if (connection.session.isOpen()){
+                if (!connection.currentUser.equals(excludeUser)){
+                    connection.send(notification.toString());
+                }
+            } else{
+                removeList.add(connection);
+            }
+        }
+        for (var connection: removeList){
+            connections.remove(connection.currentUser);
+        }
     }
 }
 
