d20ad2c5245530db663ffa5a1f9bb304894a8d0f
cthom3 <129532871+cthom3@users.noreply.github.com>
2025-04-03 00:03:21 -0600

    Added initialization of WebSocketFacade

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -5,11 +5,17 @@ import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
+import messages.Action;
+
 @WebSocket
 public class WebSocketHandler {
     private final ConnectionManager connections = new ConnectionManager();
 
     @OnWebSocketMessage
-    public void onMessage(Session session, String message){
-
+    public void onMessage(Session session, String message) {
+        Action action = new Gson().fromJson(message, Action.class);
+        switch (action.type()) {
+            case PLAYER -> enter()
+        }
+    }
 }
